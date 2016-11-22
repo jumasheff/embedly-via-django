aeb1c260495a33ad7533819d64ccc94804ec4417
Murat Jumashev <jumasheff@gmail.com>
2016-11-23 00:45:46 +0600

    Add url

--- restembed/urls.py
+++ restembed/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from restembed.core.views import save_embed
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', save_embed),
 ]
