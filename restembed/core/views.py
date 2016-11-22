f9485129d5993ea573d9a8bfd843481342d7ed62
Murat Jumashev <jumasheff@gmail.com>
2016-11-23 00:45:09 +0600

    Create function based view

--- restembed/core/views.py
+++ restembed/core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import requests
 
-# Create your views here.
+from django.shortcuts import render
+from django.conf import settings
+
+from .forms import SubmitEmbed
+from .serializer import EmbedSerializer
+
+
+def save_embed(request):
+    if request.method == 'POST':
+        form = SubmitEmbed(request.POST)
+        if form.is_valid():
+            url = form.cleaned_data['url']
+            r = requests.get('http://api.embed.ly/1/oembed?key=' + settings.EMBEDLY_KEY + '&url=' + url)
+            json = r.json()
+            serializer = EmbedSerializer(data=json)
+            if serializer.is_valid():
+                embed = serializer.save()
+                return render(request, 'embeds.html', {'embed': embed})
+    else:
+        form = SubmitEmbed()
+    return render(request, 'index.html', {'form': form})
